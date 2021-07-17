fe4d8645a3cbd423944df72526362e0f6a77da4b
Armando Zubillaga <ajzpiv97@gmail.com>
2021-07-17 12:53:28 -0400

    Added new date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,22 +4,26 @@ import Block2 from "./element/Block2";
 import Block3 from "./element/Block3";
 import styles from "./Footer.module.scss";
 
-const Footer = () => (
-  <footer style={{ backgroundColor: "rgb(255,255,255)" }}>
-    <div className={`${styles["_footer-space"]}`}>
-      <div className={`${styles["_footer-flex"]}`}>
-        <Block1 />
-        <Block2 />
-        <Block3 />
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer style={{ backgroundColor: "rgb(255,255,255)" }}>
+      <div className={`${styles["_footer-space"]}`}>
+        <div className={`${styles["_footer-flex"]}`}>
+          <Block1 />
+          <Block2 />
+          <Block3 />
+        </div>
+        <p style={{ textAlign: "center" }}>
+          <small>
+            Copyright &copy; 2017-{year} by The Hunter's Company SAS. All Rights
+            Reserved.
+          </small>
+        </p>
       </div>
-      <p style={{ textAlign: "center" }}>
-        <small>
-          Copyright &copy; 2017-2021 by The Hunter's Company SAS. All Rights
-          Reserved.
-        </small>
-      </p>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
